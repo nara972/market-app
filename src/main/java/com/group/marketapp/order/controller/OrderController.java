66e051deee866f3ee8535761f7cb913dc17d9f0d
nara972 <68370958+nara972@users.noreply.github.com>
2024-07-25 14:18:13 +0900

    redis 로그인으로 인한 주문 생성 컨트롤러 수정

--- src/main/java/com/group/marketapp/order/controller/OrderController.java
+++ src/main/java/com/group/marketapp/order/controller/OrderController.java
@@ -4,8 +4,12 @@ package com.group.marketapp.order.controller;
 import com.group.marketapp.order.dto.request.CreateOrderRequestDto;
 import com.group.marketapp.order.service.OrderService;
 import com.group.marketapp.user.domain.LoginUser;
+import com.group.marketapp.user.domain.User;
+import com.group.marketapp.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +20,17 @@ import static com.group.marketapp.common.constant.SessionConstant.SESSION_ID;
 public class OrderController {
 
     private final OrderService orderService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    @PostMapping("/order")
-    public void CreateOrder(@RequestBody CreateOrderRequestDto request, HttpServletRequest httprequest){
+    @PostMapping("/order/{loginId}")
+    public void createOrder(@PathVariable String loginId, @RequestBody CreateOrderRequestDto request) {
+        User loginUser = (User) redisTemplate.opsForValue().get("USER_SESSION_" + loginId);
 
-        LoginUser loginUser = (LoginUser) httprequest.getSession().getAttribute(SESSION_ID);
-
-        if(loginUser == null) {
+        if (loginUser == null) {
             throw new IllegalArgumentException("로그인 후 주문 가능합니다.");
         }
 
         orderService.createOrder(request, loginUser.getId());
-
     }
+
 }
