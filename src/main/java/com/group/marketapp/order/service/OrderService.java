1bdbddaeef9d8c0b2b5cc9cfb0ac8781b72f9f3b
nara972 <68370958+nara972@users.noreply.github.com>
2024-09-24 12:50:22 +0900

    비관적 락으로 인한 OrderService 수정
    비관적 락 적용으로 인한 중복 재고 확인 코드 제거

--- src/main/java/com/group/marketapp/order/service/OrderService.java
+++ src/main/java/com/group/marketapp/order/service/OrderService.java
@@ -27,43 +27,35 @@ public class OrderService {
     private final UserService userService;
     private final UserRepository userRepository;
 
-
     @Transactional
     public Long createOrder(CreateOrderRequestDto request,String loginId){
+
 
         Users user = userRepository.findByLoginId(loginId)
                 .orElseThrow(()->new IllegalArgumentException("User not found"));
 
-        //check product stock
-        for(OrderProductRequestDto dto : request.getOrderProducts()){
-            Product product = productRepository.findById(dto.getProductId())
-                    .orElseThrow(() -> new IllegalArgumentException("Product not found"));
-
-            if(product.getStock()< dto.getCount()){
-                throw new IllegalArgumentException("Product has not enough stock");
-            }
-        }
 
         Order order = request.toOrder(user);
         order = orderRepository.save(order);
 
-        System.out.println("order.getId() : " + order.getId());
-
         List<OrderProduct> orderProducts = request.toOrderProducts();
 
-        for(OrderProduct orderProduct: orderProducts){
-            orderProduct.setOrder(order);
+        for(OrderProductRequestDto dto : request.getOrderProducts()){
 
-            Product product = productRepository.findById(orderProduct.getProduct().getId())
+            Product product = productRepository.findById(dto.getProductId())
                     .orElseThrow(() -> new IllegalArgumentException("Product not found"));
 
-            product.setStock(product.getStock()-orderProduct.getCount());
+            product.setStock(product.getStock() - dto.getCount());
             productRepository.save(product);
         }
-
-        orderProductRepository.saveAll(orderProducts);
+        
+        for(OrderProduct orderProduct: orderProducts){
+            orderProduct.setOrder(order);
+            orderProductRepository.saveAll(orderProducts);
+        }
 
         return order.getId();
 
     }
+
 }
