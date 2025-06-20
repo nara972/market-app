4a73bf913aa1c486f3f53b084f75ebb5e27c08a2
nara <dlskfk9722@naver.com>
2025-06-20 19:38:51 +0900

    chore: Redis, Elasticsearch, MySQL 컨테이너 연동 및 Docker Compose 설정 완료

--- src/main/java/com/group/marketapp/common/config/ElasticsearchConfig.java
+++ src/main/java/com/group/marketapp/common/config/ElasticsearchConfig.java
@@ -4,44 +4,25 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 public class ElasticsearchConfig {
 
-    // application.yml의 설정 값을 주입받음
     @Value("${spring.elasticsearch.uris}")
     private String elasticsearchUri;
 
-    @Value("${spring.elasticsearch.username}")
-    private String username;
-
-    @Value("${spring.elasticsearch.password}")
-    private String password;
-
     @Bean
     public RestClient restClient() {
-        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(
-                AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password) 
-        );
-
         String[] uriParts = elasticsearchUri.replace("http://", "").split(":");
         String host = uriParts[0];
         int port = Integer.parseInt(uriParts[1]);
 
         return RestClient.builder(
                 new HttpHost(host, port, "http")
-        ).setHttpClientConfigCallback(httpClientBuilder ->
-                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
         ).build();
     }
 
--- src/main/java/com/group/marketapp/common/config/RedissonConfig.java
+++ src/main/java/com/group/marketapp/common/config/RedissonConfig.java
@@ -1,10 +1,14 @@
 package com.group.marketapp.common.config;
 
+import com.group.marketapp.service.RedisSubscriber;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedissonConfig {
@@ -13,9 +17,20 @@ public class RedissonConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:6379") // Redis 서버 주소
+                .setAddress("redis://redis:6379")// Redis 서버 주소
                 .setConnectionPoolSize(10)
                 .setConnectionMinimumIdleSize(2);
         return Redisson.create(config);
     }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            RedisSubscriber redisSubscriber) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(redisSubscriber, new ChannelTopic("couponNotifications"));
+        return container;
+    }
+
 }
